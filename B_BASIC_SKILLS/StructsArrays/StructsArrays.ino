17e1b9639e6f6bbc2d24c3ebacfa0d4173ab852e
CRDArcy <cdarcy@rsgc.on.ca>
2018-06-07 19:39:00 -0400

    June7

---
+++ B_BASIC_SKILLS/StructsArrays/StructsArrays.ino
@@ -0,0 +1,15 @@
+// PROJECT  : 
+// PURPOSE  : 
+// AUTHOR   :C. D'Arcy
+// DATE     :YYYY MM DD
+// uC       :328/84/85
+// STATUS   :Not Working
+// REFERENCE:?? datasheet:
+// REFERENCE:
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+}
