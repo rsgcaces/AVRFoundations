b39de16c41b9f51f55f1bca947b2f6561d7fa932
CRDArcy <cdarcy@rsgc.on.ca>
2018-06-24 18:54:57 -0400

    June 24

---
+++ BASIC_SKILLS/RGBStructs/RGBStructs.ino
@@ -0,0 +1,33 @@
+// Project  :RGBStructs
+// Purpose  :Introducing Struct data type to ACES
+// Author   :C. D'Arcy
+// Date     :2018 06 24
+// Status   :Working
+// Reference:https://playground.arduino.cc/Code/Struct
+/* Alternative definition syntax
+  typedef struct {
+  uint8_t redPin;
+  uint8_t gndPin;
+  uint8_t greenPin;
+  uint8_t bluePin;
+  } RGB1;*/
+struct RGB {        //definition of a composite data structure
+  uint8_t redPin;   //the pin number of the red LED lead
+  uint8_t gndPin;   //the common cathode pin
+  uint8_t greenPin; //green
+  uint8_t bluePin;  //blue
+};
+RGB led = {3, 5, 6};//create a single instance of an RGB struct
+void setup() {
+  pinMode(led.redPin, OUTPUT);
+  pinMode(led.gndPin, INPUT);
+  pinMode(led.greenPin, OUTPUT);
+  pinMode(led.bluePin, OUTPUT);
+  digitalWrite(led.gndPin, LOW);
+}
+void loop() {
+  analogWrite(led.redPin, 255);   //start with an initial presentation
+  analogWrite(led.greenPin, 0);
+  analogWrite(led.bluePin, 0);
+  while (true);
+}
