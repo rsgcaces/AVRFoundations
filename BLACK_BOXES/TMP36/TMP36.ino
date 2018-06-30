0f14cba6e5f6658db7f64e6d1b3bb6c0e7ff816f
CRDArcy <cdarcy@rsgc.on.ca>
2018-06-30 01:13:07 -0400

    Minor Edits

--- BLACK_BOXES/TMP36/TMP36.ino
+++ BLACK_BOXES/TMP36/TMP36.ino
@@ -1,15 +1,15 @@
 // PROJECT  :TMP36 
-// PURPOSE  :Demonstate the versatility of the TMP36 Temperature Sensor 
+// PURPOSE  :To demonstrate the versatility of the TMP36 Temperature Sensor 
 // AUTHOR   :C. D'Arcy
 // DATE     :2018 06 30
 // uC       :328
 // STATUS   :Working
 // REFERENCE:http://mail.rsgc.on.ca/~cdarcy/Datasheets/TMP35_36_37.pdf
 // REFERENCE:https://learn.adafruit.com/tmp36-temperature-sensor/using-a-temp-sensor
-
+#define DURATION 1000     //delay between readings implies frequency of readings
 uint8_t sensorPin = A0;   //use the first analog input pin, why not?
-float vRef = 5.0;       //define the voltage reference (2.7V <-> 5.0V)
 uint16_t rawADC;          //the raw 10-bit ADC reading
+float vRef = 5.0;         //define the voltage reference (2.7V <-> 5.0V)
 float tempC;              //the temperature in degrees Celsius
 
 void setup() {                //one-time prep
@@ -19,7 +19,7 @@ void setup() {                //one-time prep
 
 void loop() {
   rawADC = analogRead(sensorPin);     //obtain a reading from he TMP36
-  delay(1000);                        //pause (include time for ADC to settle)
+  delay(DURATION);                    //pause (include time for ADC to settle)
                                       //conversion: rawADC > mV > #degC > offset...
   tempC = map(rawADC, 0, 1024, 0, vRef * 1000) / 10.0 - 50.0;
   // Publish results
