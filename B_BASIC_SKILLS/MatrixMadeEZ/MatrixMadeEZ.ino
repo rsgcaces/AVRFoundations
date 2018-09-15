6e1654a23cc40e50afef2fdac0de3832dbbb51b1
CRDArcy <cdarcy@rsgc.on.ca>
2018-09-15 19:29:08 -0400

    2018 09 15

--- B_BASIC_SKILLS/MatrixMadeEZ/MatrixMadeEZ.ino
+++ B_BASIC_SKILLS/MatrixMadeEZ/MatrixMadeEZ.ino
@@ -1,32 +1,33 @@
 // PROJECT  :MatrixMadeEZ
-// PURPOSE  :Shell sketch for Hugo Reed's (ACES' 18) PCB by the same name
-//          :Draws the primary diagonal (top left to bottom right)
+// PURPOSE  :Demo program of the MatrixMadeEZ Device (original design by H. Reed)
 // AUTHOR   :C. D'Arcy
-// DATE     :2018 06 21
+// DATE     :2018 09 15
 // uC       :328
 // STATUS   :Working
 // REFERENCE:https://cdn-shop.adafruit.com/datasheets/1079datasheet.pdf
-uint8_t dim = 9;          //
-uint8_t data = 10;        //
-uint8_t clk = 11;         //
-uint8_t latch = 12;       //
-#define LEVEL 0           //
-uint8_t rowData = 0x80;   //
-uint8_t colData = rowData;//
+#define LEVEL 0
+uint8_t dimmer = 9;
+uint8_t latch = 10;
+uint8_t clk = 11;
+uint8_t data = 12;
+uint8_t colData = 0xF0;
+uint8_t rowData = 0x80;
 
 void setup() {
-  pinMode(data, OUTPUT);  //
-  pinMode(clk, OUTPUT);   //
-  pinMode(latch, OUTPUT); //
-  pinMode(dimmer,OUTPUT); //
-  analogWrite(dim,LEVEL); //
+  Serial.begin(9600);
+  pinMode(dimmer,OUTPUT);
+  pinMode(data, OUTPUT);
+  pinMode(clk, OUTPUT);
+  pinMode(latch, OUTPUT);
+  analogWrite(dimmer,LEVEL);
+
 }
 
 void loop() {
-  digitalWrite(latch, LOW);               //
-  shiftOut(data, clk, LSBFIRST, colData); //
-  shiftOut(data, clk, LSBFIRST, rowData); //
-  digitalWrite(latch, HIGH);              //
-  rowData = rowData == 1 ? rowData = 0x80 : rowData >> 1; //
-  colData = rowData;                      //
+  digitalWrite(latch, LOW);
+  shiftOut(data, clk, LSBFIRST, colData);
+  shiftOut(data, clk, LSBFIRST, rowData);
+  digitalWrite(latch, HIGH);
+  rowData = rowData == 1 ? rowData = 0x80 : rowData >> 1;
+  colData = rowData;
 }
