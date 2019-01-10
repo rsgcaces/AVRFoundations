ad4c133d0c0ea9642aaea92b15356e9eaa0534d2
CRDArcy <cdarcy@rsgc.on.ca>
2019-01-10 00:59:59 -0500

    Update SerialEchoTest.ino

--- D_DESIGN_ENGINEERING_PROJECT/SerialEchoTest/SerialEchoTest.ino
+++ D_DESIGN_ENGINEERING_PROJECT/SerialEchoTest/SerialEchoTest.ino
@@ -1,27 +1,35 @@
 // PROJECT  :SerialTest
-// PURPOSE  :Sample the behaviour of Serial Communication (UART peripheral) through the use
+// PURPOSE  :Explore the behaviour of Serial Communication (UART peripheral) through the use
 //          :of the print, println, write, and read functions of the Serial Class
-// DEVICE   :AVR 8-bit
+// DEVICE   :8-bit AVR
 // AUTHOR   :C. D'Arcy
-// DATE     :2019 01 09
+// DATE     :2019 01 10
 // uC       :328/84/85
 // COURSE   :Prepared for 2018-2019 ICS3U
 // STATUS   :Working
-// NOTE     :The Arduino UNO has only one Serial Channel (UART)
+// REFERENCE:https://www.arduino.cc/reference/en/language/functions/communication/serial/
+//          :http://www.asciitable.com/
+// NOTES    :The Arduino UNO (ATmega328p) has only ONE (hardware) Serial Channel (UART)
 //          :The role of the Serial Monitor within the Arduino IDE is to give users the
-//          :opportunity to interact (I/O) with the serial data
+//          :opportunity to interpret and interact (I/O) with the serial data stream
 void setup() {
-  Serial.begin(9600, SERIAL_8N1);  // set the communication speed and optional parameters
-  Serial.println();     // put a carriage return (CR-13) in the channel
-  Serial.write(65);     // displays the ASCII character equivalent of the number...
-  Serial.write(605);    // displays the ASCII character equivalent of the low byte of the number...
-  Serial.println(65);   // display the ASCII characters making up the number followed by a CR (13)
+  // Request the support of the AVR microcontroller's UART subsystem and set
+  // the FOUR serial comm. parameters: baud rate, #bits, parity, and #stop bits
+  Serial.begin(9600, SERIAL_8N1);
+  Serial.println();     // places the 8-bit value of 13 (carriage return) into the serial stream
+  Serial.write(65);     // places the 8-bit value (65) in the serial stream
+  Serial.write(605);    // places only the LOW byte of the number in the serial stream, discarding the rest
+  Serial.println(65);   // parses the number into as many decimal (default) digits as required
+  // and places the ASCII value of each (48-57) into the the serial stream
+  // followed by the 8-bit value of 13 (carriage return)
 }
 
-// Enter characters in the Serial Monitor and examine the different responses
+// Upload the code and launch the Serial Monitor
+//  1. Confirm the contents displayed from the setup function, and,
+//  2. Experiment with different inputs, examining the echoed response
 void loop() {
   while (Serial.available() == 0);
   uint8_t data = Serial.read();
-  //  Serial.println(data);       //describe the action this function performs
-  Serial.write(data);             //switch to this function and explain the difference
+  Serial.println(data);         //describe the action this function performs
+  //Serial.write(data);         //switch to this function and explain the difference
 }
