a52a0059d7994e56aabdbc4d9f524f178b8c6aa4
CRDArcy <cdarcy@rsgc.on.ca>
2019-01-17 13:14:58 -0500

    Blank sketch for Adafruit 746 GPS data pass through

---
+++ D_DESIGN_ENGINEERING_PROJECT/BlankGPSEcho/BlankGPSEcho.ino
@@ -0,0 +1,14 @@
+// PROJECT  :BlankGPSEcho 
+// PURPOSE  :To confirm the functionality of Adafruit 746 GPS that dumps GPS
+//          :data immediately on power up. Follow the simple direct wiring steps
+//          :at the link below... 
+// DEVICE   :UNO
+// AUTHOR   :C. D'Arcy
+// DATE     :2019 01 17
+// uC       :328
+// COURSE   :ICS3U
+// STATUS   :Not Working
+// REFERENCE:https://learn.adafruit.com/adafruit-ultimate-gps/direct-computer-wiring
+
+void setup() {}
+void loop() {}
