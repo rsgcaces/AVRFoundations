a05270ffe493e96374f8dc3f0ead35daa3ac3ca1
CRDArcy <cdarcy@rsgc.on.ca>
2019-01-24 06:02:31 -0500

    Update Chat.ino

--- D_DESIGN_ENGINEERING_PROJECT/Chat/Chat.ino
+++ D_DESIGN_ENGINEERING_PROJECT/Chat/Chat.ino
@@ -1,37 +1,43 @@
 // PROJECT  :Chat
 // PURPOSE  :Utility to enable a conversation between ACES' Arduino users
 // DEVICE   :2 Arduinos
-// AUTHOR   :C. D'Arcy
-// DATE     :2019 01 12
+// AUTHOR   :Lots of people
+// DATE     :2019 01 24
 // uC       :328
 // COURSE   :ICS3U
-// STATUS   :Working
+// STATUS   :(Possibly) Working
 // REFERENCE:https://www.arduino.cc/en/Tutorial/SoftwareSerialExample
+//          :https://www.instructables.com/id/Simple-Chat-Program-with-Arduino-Uno/
+// NOTES    :Receives from the hardware serial, sends to hardware & software serial.
+//          :Receives from software serial, sends to hardware serial.
+
 #include <SoftwareSerial.h>
-SoftwareSerial mySerial(10, 11); // RX, TX
+SoftwareSerial chat(10, 11); // RX, TX
 char ch;
 
 void setup() {
   Serial.begin(9600);     //use the hardware UART for the Serial Monitor
   while (!Serial);        //wait for serial port to connect
+  Serial.println("Starting Chat Program...");
   // set the data rate for the SoftwareSerial port
-  mySerial.begin(4800);
-  mySerial.println("Hello, world?");
+  chat.begin(9600);
+  delay(1000);
+  chat.println("Hello, world?");
   Serial.println("Ready to chat...");
 }
 
 void loop() {
-  if (mySerial.available()) {       //anything incoming?
-    Serial.write(mySerial.read());  //if so, display the oldest character in the incoming (queued) buffer
+  if (chat.available()) {           //anything incoming?
+    Serial.write(chat.read());      //if so, display the oldest character in the incoming (queued) buffer
   }
   if (Serial.available())  {        //anything outgoing?
     Serial.print("Self:\t");        //if so, let's get started...
-    while (Serial.available()) {    //transmit each of the outgoing characters... 
+    while (Serial.available()) {    //transmit each of the outgoing characters...
       ch = Serial.read();           //...to the software serial stream
-      mySerial.write(ch);           //...
+      chat.write(ch);               //...
       Serial.write(ch);             //also, echo each character on my local Serial Monitor
     }
-    mySerial.println();             //At end, transmit a newline (carriage return) character
+    chat.println();                 //At end, transmit a newline (carriage return) character
     Serial.println();               //At end, also write a newline character to the local Serial Monitor
   }
 }
