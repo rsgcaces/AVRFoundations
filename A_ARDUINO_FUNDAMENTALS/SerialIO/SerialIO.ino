d4e65c5bb62f0ea4ab39d93fc12d03fed47674ea
Chris D'Arcy <cdarcy@rsgc.on.ca>
2019-09-23 13:49:13 -0400

    2019 09 23

--- A_ARDUINO_FUNDAMENTALS/SerialIO/SerialIO.ino
+++ A_ARDUINO_FUNDAMENTALS/SerialIO/SerialIO.ino
@@ -4,19 +4,19 @@
 // DATE     :2018 06 05
 // uC       :328
 // STATUS   :Working
-uint8_t ch = 150;
+uint8_t ch = 65;
 void setup() {
   Serial.begin(9600); // initialize the serial stream to 9600 baud (bits/s)
 }
-
 void loop() {
-//  while (!Serial.available());    //wait for a character to be entered
-//  ch = Serial.read();             //ok, grab it...
+  //while (!Serial.available());    //wait for a character to be entered
+  //ch = Serial.read();             //ok, grab it...
   Serial.println((char)ch);       //cast (force) the data to appear as an ASCII Character
   Serial.println(ch);             //unmodified, it appears in decimal form
   Serial.println(ch, DEC);        //explicitly request presentation as a base 10 number
   Serial.println(ch, BIN);        //explicitly request presentation as a base 2 number
   Serial.println(ch, OCT);        //explicitly request presentation as a base 8 number
   Serial.println(ch, HEX);        //explicitly request presentation as a base 16 number
+  Serial.write(ch);
   while (true);                   //wait here...
 }
