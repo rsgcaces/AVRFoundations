af74d170139d9e300caf735dbbbbc0ee00dd4f9f
Chris D'Arcy <cdarcy@rsgc.on.ca>
2019-10-16 12:22:40 -0400

    Update BinaryCounter.ino

--- A_ARDUINO_FUNDAMENTALS/BinaryCounter/BinaryCounter.ino
+++ A_ARDUINO_FUNDAMENTALS/BinaryCounter/BinaryCounter.ino
@@ -4,9 +4,10 @@
 // STATUS   :Working
 uint8_t clockPin = 13;                          //to pin 11 of '595
 uint8_t powerPin = 12;                          //provide 5V power to '595
-uint8_t latchPin = 10;                          //to pin 12 of '595
 uint8_t dataPin = 11;                           //to pin 14 of '595
-uint8_t DURATION = 500;                         //frame delay interval
+uint8_t latchPin = 10;                          //to pin 12 of '595
+uint8_t outputEnable = 9;                       //to pin 13 of '595
+uint8_t DURATION = 100;                         //frame delay interval
 uint8_t n = 0;                                  //declare and initialize counter
 
 void setup() {
@@ -15,10 +16,12 @@ void setup() {
   pinMode(dataPin, OUTPUT);                     //  as outputs
   pinMode(powerPin, OUTPUT);                    //provide 5V power source
   digitalWrite(powerPin, HIGH);                 //  to '595 (not the best)
+  pinMode(outputEnable, OUTPUT);                //'595 output resgisters only 
+  digitalWrite(outputEnable, LOW);              //  visible if pin 13 kept low
 }
 void loop() {
   digitalWrite(latchPin, LOW);                  //suppress external presentation
-  shiftOut(dataPin, clockPin, LSBFIRST, n++);   //8-bit serial in...
+  shiftOut(dataPin, clockPin, MSBFIRST, n++);   //8-bit serial in...
   digitalWrite(latchPin, HIGH);                 //present contents of 8 internal registers
   delay(DURATION);                              //pause to confirm
 }
