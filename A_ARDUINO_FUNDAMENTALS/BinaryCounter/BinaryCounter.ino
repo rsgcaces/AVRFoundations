46af1f371113f2067bcf5c789e3d16f1dfb31bdc
Chris D'Arcy <cdarcy@rsgc.on.ca>
2019-10-16 09:14:13 -0400

    Update BinaryCounter.ino

--- A_ARDUINO_FUNDAMENTALS/BinaryCounter/BinaryCounter.ino
+++ A_ARDUINO_FUNDAMENTALS/BinaryCounter/BinaryCounter.ino
@@ -1,22 +1,21 @@
-// PROJECT  :Binary Counter
+// PROJECT  :BinaryCounter
 // PURPOSE  :To confirm T. Morland's Shift-Bar Device
-// DATE     :2018 06 04
+// DATE     :2019 10 17
 // STATUS   :Working
-#define clockPin 9                              //
-#define latchPin 10                             //
-#define dataPin 11                              //
-#define DURATION 500                            //
-uint8_t n = 0;                                  //
+uint8_t clockPin = 13;                          //to pin 11 of '595
+uint8_t latchPin = 10;                          //to pin 12 of '595
+uint8_t dataPin = 11;                           //to pin 14 of '595
+uint8_t DURATION = 500;                         //frame delay interval
+uint8_t n = 0;                                  //declare and initialize counter
+
 void setup() {
-  pinMode(clockPin, OUTPUT);                    //
-  pinMode(latchPin, OUTPUT);                    //
-  pinMode(dataPin, OUTPUT);                     //
-  Serial.begin(9600);                           //
-  while (!Serial);                              //
+  pinMode(clockPin, OUTPUT);                    //declare pins for 
+  pinMode(latchPin, OUTPUT);                    //  shift register signal lines
+  pinMode(dataPin, OUTPUT);                     //  as outputs
 }
 void loop() {
-  digitalWrite(latchPin, LOW);                  //
-  shiftOut(dataPin, clockPin, LSBFIRST, n++);   //
-  digitalWrite(latchPin, HIGH);                 //
-  delay(DURATION);                              //
+  digitalWrite(latchPin, LOW);                  //suppress external presentation
+  shiftOut(dataPin, clockPin, LSBFIRST, n++);   //8-bit serial in...
+  digitalWrite(latchPin, HIGH);                 //present contents of 8 internal registers
+  delay(DURATION);                              //pause to confirm
 }
