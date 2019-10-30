48fca7b416473ac77093d3af342907b1025f1bf1
Chris D'Arcy <cdarcy@rsgc.on.ca>
2019-10-30 10:01:01 -0400

    Update Dual7SegmentPOV.ino

--- B_BASIC_SKILLS/Dual7SegmentPOV/Dual7SegmentPOV.ino
+++ B_BASIC_SKILLS/Dual7SegmentPOV/Dual7SegmentPOV.ino
@@ -1,6 +1,6 @@
 // PROJECT  :Dual7SegmentPOV
-// PURPOSE  :Demonstrates PoV on Rohm's LB-602MK2 Dual 7 Segment Display
-//          : by continually counting from 0 to 255 in HEX
+// PURPOSE  :Demonstrates PoV on ROHM's LB-602MK2 Dual 7 Segment Display
+//          :by counting continuously
 // COURSE   :ICS3U
 // AUTHOR   :In class
 // DATE     :2019 10 31
@@ -12,8 +12,11 @@ uint8_t clockPin = 13;    //best associated with SCK
 uint8_t dataPin = 11;     //best associated with MOSI
 uint8_t latchPin = 10;    //best associated with SS
 uint8_t brightPin = 9;    //PWM for '595 brightness control on /OE pin
+
 #define LEVEL 240;        //PWM: 0-full on, 255-full off (non-linear)
-uint8_t hexMap[] = {
+#define BASE 10           //decimal, for now..
+
+uint8_t digitalMap[] = {
   //ABCDEFG.              //to be defined...
   0b00000000, //0
   0b00000000, //1
@@ -24,18 +27,13 @@ uint8_t hexMap[] = {
   0b00000000, //6
   0b00000000, //7
   0b00000000, //8
-  0b00000000, //9
-  0b00000000, //A
-  0b00000000, //b
-  0b00000000, //C
-  0b00000000, //d
-  0b00000000, //E
-  0b00000000};//F
+  0b00000000};//9
 
   void setup() {
     pinMode(clockPin, OUTPUT);
     pinMode(dataPin, OUTPUT);
     pinMode(latchPin, OUTPUT);
+    pinMode(brightPin, OUTPUT);
   }
 
   void loop() {
