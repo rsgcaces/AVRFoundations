7a5c517e0536b408f8c988b001bb535a13694e5c
Chris D'Arcy <cdarcy@rsgc.on.ca>
2025-03-18 09:59:39 -0400

    Updated for 2025

--- D_DESIGN_ENGINEERING_PROJECT/RxTest/RxTest.ino
+++ D_DESIGN_ENGINEERING_PROJECT/RxTest/RxTest.ino
@@ -1,41 +1,35 @@
 // PROJECT  :RxTest
-// PURPOSE  :To display an incoming byte from TxTemp on a Morland bargraph
-// DEVICE   :Arduino + ACES nRF Breakout Board + nRF24L01
+// PURPOSE  :To display an incoming byte from TxTemp on a Morland Bargraph V3
+// DEVICE   :Nano + ACES nRF Breakout Board + nRF24L01 + MBV3
 // AUTHOR   :C. D'Arcy
-// DATE     :2022 04 22
+// DATE     :Created: 2019 02 09. Updated: 2025 03 18.
 // uC       :328p
-// COURSE   :ICS3U
+// COURSE   :ICS3U-E
 // STATUS   :Working (with TxTest)
 // REFERENCE:http://darcy.rsgc.on.ca/ACES/TEI3M/CommunicationProtocols.html#RF1
 //          :https://tmrh20.github.io/RF24/classRF24.html
 //          :http://darcy.rsgc.on.ca/ACES/Datasheets/nRF24L01.pdf
+#include <SPI.h>        //not necessary
 #include <RF24.h>
-#define CHANNEL 100             //any value between 0 and 125 (2.400GHz to 2.525GHz)
-uint8_t receivedData;           //for receiver to store the incoming byte
-uint8_t groundPin = 14;         //aka A0 (PC0)
-uint8_t clockPin = 15;          //aka A1 (PC1)
-uint8_t powerPin = 16;          //aka A2 (PC2)
-uint8_t dataPin = 17;           //aka A3 (PC3)
-uint8_t latchPin = 18;          //aka A4 (PC4)
-uint8_t enablePin = 19;         //aka A5 (PC5)
+#define CHANNEL 93      //value between 0 and 125 (2.400GHz to 2.525GHz)
+uint8_t receivedData;   //for receiver to store the incoming byte
+uint8_t clockPin = A1;  //aka D15 (PC1). MBV3(CLK)
+uint8_t dataPin = A3;   //aka D17 (PC3). MBV3(DATA)
+uint8_t latchPin = A4;  //aka D18 (PC4). MBV3(LATCH)
 
-
-RF24 radio(9, 10);                            //CE (Chip Enable/Disable), CSN(SS)
+RF24 radio(9, 10);      //CE (Chip Enable/Disable), CSN(SS)
 uint8_t addresses[][6] = {"1Node", "2Node"};  //pipe names (See Reference 2)
 
 void setup() {
-  Serial.begin(9600);           //invoke the local terminal window
-  while (!Serial);              //wait for full activation
-  configureBargraph();          //set up the Morland bargraph
- //The next two statement are optional (confirmation only)
-  displayData(255);             //confirm display working...
-  delay(3000);                  //pause
-  radio.begin();                //invoke the radio
+  Serial.begin(9600);   //invoke the local terminal window
+  while (!Serial);      //wait for full activation
+  configureBargraph();  //set up the Morland Bargraph V3
+
+  radio.begin();        //invoke the radio
   radio.setPALevel(RF24_PA_LOW);//set low power amplification for close proximity
   radio.setChannel(CHANNEL);    //mutually agreeable frequency offset(2.400GHz to 2.525GHz)
 
   radio.openWritingPipe(addresses[1]);    //select ONE addresses to write to
-  //https://tmrh20.github.io/RF24/classRF24.html#a9edc910ccc1ffcff56814b08faca5535
   radio.openReadingPipe(1, addresses[0]); //receiver and transmit are reversed
   radio.startListening();                 //should be good to go...
 }
@@ -49,23 +43,15 @@ void loop() {
 }
 
 void displayData(uint8_t rec) {
-  uint8_t data = (1 << (rec >> 5)) - 1;         //What do you think this does?
-  digitalWrite(latchPin, LOW);                  //detach shift regs from storage register
+  uint8_t data = (1 << ((rec >> 5)+1)) - 1;     //What do you think this does?
+  digitalWrite(latchPin, LOW);                  //detach shift regs from storage registers
   shiftOut(dataPin, clockPin, LSBFIRST, data);  //next, shift bits in, serially....
-  digitalWrite(latchPin, HIGH);                 //connect shift regs to storage register
+  digitalWrite(latchPin, HIGH);                 //connect shift regs to storage registers
 }
 
 //Our Rx display device is a Morland Bargraph inserted into PC0-PC5
 void configureBargraph() {
-  //DDRC = B11111000;                 //preferred register alternative to next 5 statements
-  pinMode(clockPin, OUTPUT);          //declare all 5 Morland pins for output
-  pinMode(latchPin, OUTPUT);          //
-  pinMode(dataPin, OUTPUT);           //
-  pinMode(groundPin, OUTPUT);         //
-  pinMode(powerPin, OUTPUT);          //
-  pinMode(enablePin, OUTPUT);          //
-  digitalWrite(groundPin, LOW);       //sink
-  digitalWrite(enablePin, LOW);       //sink
-  //PORTC |= 1<<PC4;                  //preferred register alternative to next statement
-  digitalWrite(powerPin, HIGH);       //source
+  pinMode(clockPin, OUTPUT);    //declare required Morland pins for output
+  pinMode(latchPin, OUTPUT);    //wire GND, 5V and ENABLE to Supply Rails 
+  pinMode(dataPin, OUTPUT);     //
 }
--- D_DESIGN_ENGINEERING_PROJECT/TxTest/TxTest.ino
+++ D_DESIGN_ENGINEERING_PROJECT/TxTest/TxTest.ino
@@ -2,37 +2,40 @@
 // PURPOSE  :To continually transmit a byte-scaled reading from a trim pot
 // DEVICE   :2x(Arduino + nrF24L01)
 // AUTHOR   :C. D'Arcy
-// DATE     :Updated: 2022 04 22
-// uC       :328p
+// DATE     :2019 02 09. Confirmed: 2025 03 18.
+// uC       :328p (Nano)
 // COURSE   :ICS3U
 // STATUS   :Working (with RxTest)
 // REFERENCE:http://darcy.rsgc.on.ca/ACES/TEI3M/CommunicationProtocols.html#RF1
 //          :https://tmrh20.github.io/RF24/classRF24.html
 //          :http://darcy.rsgc.on.ca/ACES/Datasheets/nRF24L01.pdf
+
+#include <SPI.h>        //not necessary
 #include <RF24.h>
-RF24 radio(9, 10);              //CE (Chip Enable/Disable), CSN(SS)
+RF24 radio(9, 10);      //CE (Chip Enable/Disable), CSN(SS)
 byte addresses[][6] = {"1Node", "2Node"};
-#define CHANNEL 100             //Any value between 0 and 125 (2.400GHz to 2.525GHz)
+#define CHANNEL 93      //Any value between 0 and 125 (2.400GHz to 2.525GHz)
 //Trim pot details
-uint8_t potVal;                 //byte-scaled value read from the potentiometer
-uint8_t APin = 14;              //aka A0
-uint8_t CPin = 15;              //aka A1
-uint8_t BPin = 16;              //aka A2
+uint8_t potVal;         //byte-scaled value read from the potentiometer
+uint8_t APin = 14;      //aka A0
+uint8_t CPin = 15;      //aka A1
+uint8_t BPin = 16;      //aka A2
 
 void setup() {
-  Serial.begin(9600);                     //log the activity
-  configurePotentiometer();               //set the pot up as a voltage divider on PORT C
-  radio.begin();                          //invoke the radio object
-  radio.setPALevel(RF24_PA_LOW);          //close range so minimum power sufficient
-  radio.setChannel(CHANNEL);              //Tx and Rx communication on same channel
-  //https://tmrh20.github.io/RF24/classRF24.html#af2e409e62d49a23e372a70b904ae30e1
+  Serial.begin(9600);             //log the activity
+  configurePotentiometer();       //set the pot up as a voltage divider on PORT C
+  
+  radio.begin();                  //invoke the radio object
+  radio.setPALevel(RF24_PA_LOW);  //close range so minimum power sufficient
+  radio.setChannel(CHANNEL);      //Tx and Rx communication on same channel
+  //https://nrf24.github.io/RF24/classRF24.html
   radio.openWritingPipe(addresses[0]);    //Transmit assumes these pipes
-  radio.openReadingPipe(1, addresses[1]); //...
+  radio.stopListening();
 }
 
 void loop() {
-  potVal = analogRead(CPin) >> 2;         //obtain a pot reading [0,1023] and scale it
-  radio.write(&potVal, sizeof(int8_t));   //transmit the scaled byte [0.255]
+  potVal = analogRead(CPin) >> 2;         //obtain a pot reading [0,1023] and scale it to [0.255]
+  radio.write(&potVal, sizeof(int8_t));   //transmit the scaled byte 
   Serial.println(potVal);                 //echo to serial monitor
 }
 
